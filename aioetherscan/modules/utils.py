fa1c393229368cb91c771b986b755ce7e5df07f2
lechevalier <lechevalier@users.noreply.github.com>
2023-02-22 16:48:32 +0000

    refactor: Mutualize logic to handle pagination in utils and extend it to all paginated methods

--- aioetherscan/modules/utils.py
+++ aioetherscan/modules/utils.py
@@ -1,58 +1,152 @@
-from typing import Tuple, Dict, List, Iterator, AsyncIterator, Optional
+from itertools import count, tee
+from typing import (
+    TYPE_CHECKING, Any, TypeVar, Union, Optional, Callable,
+    Tuple, List, Iterable, Iterator, AsyncIterator, Awaitable,
+)
 from urllib.parse import urljoin, urlencode
 
 from asyncio_throttle import Throttler
 
 from aioetherscan.exceptions import EtherscanClientApiError
+if TYPE_CHECKING:
+    from ..client import Client
+
+T = TypeVar("T")
+Ordered = Union[T, List[T], Tuple[T, ...]]
+
+
+class BaseProxy:
+    """Helper methods to iterate through pagination."""
+
+    def __init__(self, client: "Client"):
+        self._client = client
+        # Defaulting to free API key rate limit
+        self._throttler = Throttler(rate_limit=5, period=1.0)
+
+    @property
+    def throttler(self) -> Throttler:
+        return self._throttler
+
+    @throttler.setter
+    def throttler(self, value: Throttler):
+        if isinstance(value, Throttler):
+            self._throttler = value
+
+    @staticmethod
+    def generate_intervals(from_number: int, to_number: int, count_: int) -> Iterator[Tuple[int, int]]:
+        for i in range(from_number, to_number + 1, count_):
+            yield (i, min(i + count_ - 1, to_number))
+
+    def proxy(self, func: Callable[..., Awaitable[Iterable[T]]]):
+
+        async def wrapper(*args, **kwargs) -> AsyncIterator[T]:
+
+            async with self.throttler:
+                result = await func(*args, **kwargs)
+            for v in result:
+                yield v
+
+        return wrapper
+
+    def parametrize(
+        self,
+        argnames: Ordered[str],
+        argvalues: Iterable[Ordered[Any]],
+        stop_if_empty: bool = True,
+    ):
+        if isinstance(argnames, str):
+            argnames = argnames.split(',')
+
+        if len(argnames) == 1:
+            argvalues = ((v,) for v in argvalues)
+
+        def inner(func: Callable[..., AsyncIterator[T]]):
+
+            async def wrapper(*args, **kwargs) -> AsyncIterator[T]:
+                nonlocal argvalues
+                argvalues, helper = tee(argvalues)
+                for values in helper:
+                    kwargs.update(zip(argnames, values))
+                    try:
+                        async for v in func(*args, **kwargs):
+                            yield v
+                    except EtherscanClientApiError as exc:
+                        if exc.message != 'No transactions found':
+                            raise
+                        if stop_if_empty:
+                            return
+                        else:
+                            continue
+
+            return wrapper
+
+        return inner
+
+    async def parse_by_pages(
+        self,
+        func: Callable[..., AsyncIterator[T]],
+        *args,
+        **kwargs,
+    ) -> AsyncIterator[T]:
+        @self.parametrize("page", count(1))
+        async def _proxy(*arg, **kwarg):
+            async for v in self.proxy(func)(*arg, **kwarg):
+                yield v
+
+        async for v in _proxy(*args, **kwargs):
+            yield v
+
+    async def parse_by_blocks(
+        self,
+        func: Callable[..., AsyncIterator[T]],
+        *args,
+        start_block: int = 0,
+        end_block: int = None,
+        block_limit: int = 10_000,
+        **kwargs,
+    ) -> AsyncIterator[T]:
+
+        if end_block is None:
+            end_block = int(await self._client.proxy.block_number(), 16)
+
+        @self.parametrize(
+            "start_block,end_block",
+            self.generate_intervals(start_block, end_block, block_limit),
+            stop_if_empty=False,
+        )
+        async def _proxy(*arg, **kwarg):
+            async for v in self.parse_by_pages(func, *arg, **kwarg):
+                yield v
+
+        async for v in _proxy(*args, **kwargs):
+            yield v
+
+
+class AccountProxy(BaseProxy):
+    async def normal_txs_generator(self, *args, **kwargs):
+        async for v in self.parse_by_blocks(self._client.account.normal_txs, *args, **kwargs):
+            yield v
+
+    async def internal_txs_generator(self, *args, **kwargs):
+        async for v in self.parse_by_blocks(self._client.account.internal_txs, *args, **kwargs):
+            yield v
+
+    async def token_transfers_generator(self, *args, **kwargs):
+        async for v in self.parse_by_blocks(self._client.account.token_transfers, *args, **kwargs):
+            yield v
+
+    async def mined_blocks_generator(self, *args, **kwargs):
+        async for v in self.parse_by_pages(self._client.account.mined_blocks, *args, **kwargs):
+            yield v
 
 
 class Utils:
     """Helper methods which use the combination of documented APIs."""
 
-    def __init__(self, client, base_url):
+    def __init__(self, client: "Client", base_url: str):
         self._client = client
         self._BASE_URL = base_url
-
-    async def token_transfers_generator(
-            self,
-            address: str = None,
-            contract_address: str = None,
-            throttler: Throttler = None,
-            block_limit: int = 50,
-            offset: int = 3,
-            start_block: int = 0,
-            end_block: int = None,
-    ) -> AsyncIterator[Dict]:
-        if end_block is None:
-            end_block = int(await self._client.proxy.block_number(), 16)
-
-        if throttler is None:
-            throttler = Throttler(rate_limit=5, period=1.0)
-
-        for sblock, eblock in self._generate_intervals(start_block, end_block, block_limit):
-            async for transfer in self._parse_by_pages(
-                    address=address,
-                    contract_address=contract_address,
-                    start_block=sblock,
-                    end_block=eblock,
-                    offset=offset,
-                    throttler=throttler
-            ):
-                yield transfer
-
-    async def token_transfers(
-            self,
-            address: str = None,
-            contract_address: str = None,
-            be_polite: bool = True,
-            block_limit: int = 50,
-            offset: int = 3,
-            start_block: int = 0,
-            end_block: int = None,
-            throttler: Throttler = None,
-    ) -> List[Dict]:
-        kwargs = {k: v for k, v in locals().items() if k != 'self' and not k.startswith('_')}
-        return [t async for t in self.token_transfers_generator(**kwargs)]
+        self.account_proxy = AccountProxy(client)
 
     async def is_contract(self, address: str) -> bool:
         try:
@@ -91,42 +185,6 @@ class Utils:
                     raise
 
         return next((i['from'].lower() for i in response), None)
-
-    async def _parse_by_pages(
-            self,
-            start_block: int,
-            end_block: int,
-            offset: int,
-            throttler: Throttler = None,
-            address: str = None,
-            contract_address: str = None,
-    ) -> AsyncIterator[Dict]:
-        page = 1
-
-        while True:
-            async with throttler:
-                try:
-                    transfers = await self._client.account.token_transfers(
-                        address=address,
-                        contract_address=contract_address,
-                        start_block=start_block,
-                        end_block=end_block,
-                        page=page,
-                        offset=offset
-                    )
-                except EtherscanClientApiError as e:
-                    if e.message == 'No transactions found':
-                        break
-                    raise
-                else:
-                    for transfer in transfers:
-                        yield transfer
-                    page += 1
-
-    @staticmethod
-    def _generate_intervals(from_number: int, to_number: int, count: int) -> Iterator[Tuple[int, int]]:
-        for i in range(from_number, to_number + 1, count):
-            yield (i, min(i + count - 1, to_number))
 
     def get_address_link(self, address: str) -> str:
         return urljoin(self._BASE_URL, f'address/{address}')
--- aioetherscan/tests/test_utils.py
+++ aioetherscan/tests/test_utils.py
@@ -1,8 +1,8 @@
 from unittest.mock import patch, call
+from itertools import count
 
 import asynctest
 import pytest
-from asyncio_throttle import Throttler
 from asynctest import CoroutineMock, MagicMock
 
 from aioetherscan import Client
@@ -10,196 +10,33 @@ from aioetherscan.exceptions import EtherscanClientApiError
 
 
 @pytest.fixture()
-async def utils():
+async def client():
     c = Client('TestApiKey')
-    yield c.utils
+    yield c
     await c.close()
 
 
 @pytest.fixture()
-async def throttler():
-    t = Throttler(rate_limit=5)
-    yield t
+async def utils(client):
+    yield client.utils
 
 
-def test_generate_intervals(utils):
+@pytest.fixture()
+async def account_proxy(client):
+    yield client.utils.account_proxy
+
+
+def test_generate_intervals(account_proxy):
     expected = [(1, 3), (4, 6), (7, 9), (10, 10)]
-    for i, j in utils._generate_intervals(1, 10, 3):
+    for i, j in account_proxy.generate_intervals(1, 10, 3):
         assert (i, j) == expected.pop(0)
 
     expected = [(1, 2), (3, 4), (5, 6)]
-    for i, j in utils._generate_intervals(1, 6, 2):
+    for i, j in account_proxy.generate_intervals(1, 6, 2):
         assert (i, j) == expected.pop(0)
 
-    for i, j in utils._generate_intervals(10, 0, 3):
+    for i, j in account_proxy.generate_intervals(10, 0, 3):
         assert True == False  # not called
-
-
-@pytest.mark.asyncio
-async def test_parse_by_pages(utils, throttler):
-    with patch('aioetherscan.modules.account.Account.token_transfers', new=CoroutineMock()) as transfers_mock:
-        transfers_mock.side_effect = EtherscanClientApiError('No transactions found', None)
-        async for _ in utils._parse_by_pages(
-                100,
-                200,
-                5,
-                address='address',
-                contract_address='contract_address',
-                throttler=throttler
-        ):
-            break
-        transfers_mock.assert_called_once_with(
-            address='address',
-            contract_address='contract_address',
-            start_block=100,
-            end_block=200,
-            page=1,
-            offset=5
-        )
-
-
-@pytest.mark.asyncio
-async def test_parse_by_pages_exception(utils, throttler):
-    with patch('aioetherscan.modules.account.Account.token_transfers', new=CoroutineMock()) as transfers_mock:
-        transfers_mock.side_effect = EtherscanClientApiError('other msg', None)
-        try:
-            async for _ in utils._parse_by_pages(
-                    100,
-                    200,
-                    5,
-                    address='address',
-                    contract_address='contract_address',
-                    throttler=throttler
-            ):
-                break
-        except EtherscanClientApiError as e:
-            assert e.message == 'other msg'
-
-
-@pytest.mark.asyncio
-async def test_parse_by_pages_result(utils, throttler):
-    def token_transfers_side_effect_generator():
-        yield [1]
-        yield [2]
-        raise EtherscanClientApiError('No transactions found', None)
-
-    gen = token_transfers_side_effect_generator()
-
-    def token_transfers_side_effect(**kwargs):
-        for x in gen:
-            return x
-
-    with patch('aioetherscan.modules.account.Account.token_transfers', new=CoroutineMock()) as transfers_mock:
-        transfers_mock.side_effect = token_transfers_side_effect
-
-        i = 0
-        res = []
-        async for transfer in utils._parse_by_pages(
-                100,
-                200,
-                5,
-                address='address',
-                contract_address='contract_address',
-                throttler=throttler
-        ):
-            i += 1
-            if i > 2:
-                break
-            res.append(transfer)
-        transfers_mock.assert_has_calls(
-            [
-                call(
-                    address='address',
-                    contract_address='contract_address',
-                    start_block=100,
-                    end_block=200,
-                    page=1,
-                    offset=5
-                ),
-                call(
-                    address='address',
-                    contract_address='contract_address',
-                    start_block=100,
-                    end_block=200,
-                    page=2,
-                    offset=5
-                ),
-                call(
-                    address='address',
-                    contract_address='contract_address',
-                    start_block=100,
-                    end_block=200,
-                    page=3,
-                    offset=5
-                ),
-            ]
-        )
-        assert res == [1, 2]
-
-
-@pytest.mark.asyncio
-async def test_token_transfers(utils):
-    with asynctest.patch(
-            'aioetherscan.modules.utils.Utils.token_transfers_generator',
-            new=MagicMock()
-    ) as transfers_gen_mock:
-        await utils.token_transfers(
-            contract_address='contract_address'
-        )
-        transfers_gen_mock.assert_called_once_with(
-            contract_address='contract_address',
-            address=None,
-            be_polite=True,
-            block_limit=50,
-            offset=3,
-            start_block=0,
-            end_block=None,
-            throttler=None
-        )
-
-
-@pytest.mark.asyncio
-async def test_token_transfers_generator(utils, throttler):
-    with patch('aioetherscan.modules.utils.Utils._parse_by_pages', new=MagicMock()) as parse_mock:
-        with patch('aioetherscan.modules.proxy.Proxy.block_number', new=CoroutineMock()) as proxy_mock:
-            proxy_mock.return_value = '0x14'
-
-            async for _ in utils.token_transfers_generator(address='addr', throttler=throttler):
-                break
-
-            parse_mock.assert_called_once_with(
-                address='addr',
-                contract_address=None,
-                start_block=0,
-                end_block=20,
-                offset=3,
-                throttler=throttler
-            )
-
-    with patch('aioetherscan.modules.utils.Utils._parse_by_pages', new=MagicMock()) as parse_mock:
-        async for _ in utils.token_transfers_generator(
-                contract_address='contract_address',
-                end_block=20,
-                throttler=throttler
-        ):
-            break
-
-        parse_mock.assert_called_once_with(
-            address=None,
-            contract_address='contract_address',
-            start_block=0,
-            end_block=20,
-            offset=3,
-            throttler=throttler
-        )
-
-
-@pytest.mark.asyncio
-async def test_one_of_addresses_is_supplied(utils):
-    EXC_MESSAGE_RE = r'At least one of address or contract_address must be specified.'
-    with pytest.raises(ValueError, match=EXC_MESSAGE_RE) as excinfo:
-        async for _ in utils.token_transfers_generator(end_block=1):
-            break
 
 
 @pytest.mark.asyncio
@@ -335,3 +172,288 @@ def test_get_block_link(api_kind, network_name, block_number, expected):
 def test_get_block_txs_link(api_kind, network_name, block_number, expected):
     c = Client('TestApiKey', api_kind=api_kind, network=network_name)
     assert c.utils.get_block_txs_link(block_number) == expected
+
+
+class TestBaseProxy:
+    @pytest.mark.asyncio
+    async def test_proxy(self, account_proxy):
+        mock = CoroutineMock(return_value=[1, 2])
+        actual = [v async for v in account_proxy.proxy(mock)(
+            'foo',
+            test='bar',
+        )]
+
+        mock.assert_awaited_once_with('foo', test='bar')
+        assert actual == [1, 2]
+
+    @pytest.mark.asyncio
+    async def test_proxy_empty_result(self, account_proxy):
+        mock = CoroutineMock(side_effect=EtherscanClientApiError(
+            'No transactions found', None))
+        with pytest.raises(EtherscanClientApiError, match=r"\[No transactions found\] None"):
+            async for _ in account_proxy.proxy(mock)(
+                'foo',
+                test='bar',
+            ):
+                pass
+
+        mock.assert_awaited_once_with('foo', test='bar')
+
+    @pytest.mark.asyncio
+    async def test_proxy_exception(self, account_proxy):
+        mock = CoroutineMock(
+            side_effect=EtherscanClientApiError('Error', None))
+        with pytest.raises(EtherscanClientApiError, match=r"\[Error\] None"):
+            async for _ in account_proxy.proxy(mock)(
+                'foo',
+                test='bar',
+            ):
+                pass
+
+        mock.assert_called_once_with('foo', test='bar')
+
+    @pytest.mark.asyncio
+    @pytest.mark.parametrize("stop_if_empty", [False, True])
+    async def test_parametrize_exception(self, account_proxy, stop_if_empty):
+        def side_effect_generator():
+            yield EtherscanClientApiError('Another error', None)
+            yield []  # Should not reach this line
+
+        mock = CoroutineMock(side_effect=side_effect_generator())
+
+        @account_proxy.parametrize("page", range(1, 3), stop_if_empty=stop_if_empty)
+        async def _proxy(*arg, **kwarg):
+            async for v in account_proxy.proxy(mock)(*arg, **kwarg):
+                yield v
+
+        with pytest.raises(EtherscanClientApiError, match=r"\[Another error\] None"):
+            async for _ in _proxy(
+                'foo',
+                test='bar',
+            ):
+                pass
+
+        mock.assert_awaited_once_with('foo', test='bar', page=1)
+
+    @pytest.mark.asyncio
+    async def test_parametrize_stop_if_empty(self, account_proxy):
+        def side_effect_generator():
+            yield []
+            yield []
+            yield EtherscanClientApiError('No transactions found', None)
+            yield []
+
+        mock = CoroutineMock(side_effect=side_effect_generator())
+
+        @account_proxy.parametrize("page", count(1))
+        async def _proxy(*arg, **kwarg):
+            async for v in account_proxy.proxy(mock)(*arg, **kwarg):
+                yield v
+
+        async for _ in _proxy(
+            'foo',
+            test='bar',
+        ):
+            pass
+        assert mock.await_count == 3
+        mock.assert_has_awaits([
+            call('foo', test='bar', page=1),
+            call('foo', test='bar', page=2),
+            call('foo', test='bar', page=3),
+        ])
+
+    @pytest.mark.asyncio
+    async def test_parametrize_ignore_error(self, account_proxy):
+        def side_effect_generator():
+            yield []
+            yield []
+            yield EtherscanClientApiError('No transactions found', None)
+            yield []
+            yield []  # Should not reach this line
+
+        mock = CoroutineMock(side_effect=side_effect_generator())
+
+        @account_proxy.parametrize("page", range(1, 5), stop_if_empty=False)
+        async def _proxy(*arg, **kwarg):
+            async for v in account_proxy.proxy(mock)(*arg, **kwarg):
+                yield v
+
+        async for _ in _proxy(
+            'foo',
+            test='bar',
+        ):
+            pass
+        assert mock.await_count == 4
+        mock.assert_has_awaits([
+            call('foo', test='bar', page=1),
+            call('foo', test='bar', page=2),
+            call('foo', test='bar', page=3),
+            call('foo', test='bar', page=4),
+        ])
+
+    @pytest.mark.asyncio
+    async def test_parse_by_pages(self, account_proxy):
+        def side_effect_generator():
+            yield [1, 2]
+            yield [3]
+            yield EtherscanClientApiError('No transactions found', None)
+            yield [4]
+
+        mock = CoroutineMock(side_effect=side_effect_generator())
+
+        result = [v async for v in account_proxy.parse_by_pages(
+            mock,
+            'foo',
+            test='bar',
+        )]
+        assert mock.await_count == 3
+        mock.assert_has_awaits([
+            call('foo', test='bar', page=1),
+            call('foo', test='bar', page=2),
+            call('foo', test='bar', page=3),
+        ])
+        assert result == [1, 2, 3]
+
+    @pytest.mark.asyncio
+    async def test_parse_by_blocks(self, account_proxy):
+        def side_effect_generator():
+            yield [1, 2]
+            yield [3]
+            yield EtherscanClientApiError('No transactions found', None)
+            yield [4]
+            yield EtherscanClientApiError('No transactions found', None)
+
+        mock = CoroutineMock(side_effect=side_effect_generator())
+
+        with asynctest.patch(
+            'aioetherscan.modules.proxy.Proxy.block_number',
+            new=CoroutineMock(return_value="0x5")
+        ):
+            result = [v async for v in account_proxy.parse_by_blocks(
+                mock,
+                'foo',
+                test='bar',
+                start_block=2,
+                end_block=None,
+                block_limit=2,
+            )]
+        assert mock.await_count == 5
+        mock.assert_has_awaits([
+            call('foo', test='bar', start_block=2, end_block=3, page=1),
+            call('foo', test='bar', start_block=2, end_block=3, page=2),
+            call('foo', test='bar', start_block=2, end_block=3, page=3),
+            call('foo', test='bar', start_block=4, end_block=5, page=1),
+            call('foo', test='bar', start_block=4, end_block=5, page=2),
+        ])
+        assert result == [1, 2, 3, 4]
+
+
+class TestAccountProxy:
+    @pytest.mark.asyncio
+    async def test_mined_blocks_generator(self, client, account_proxy):
+        mock = MagicMock()
+        with asynctest.patch(
+            'aioetherscan.modules.utils.BaseProxy.parse_by_pages',
+            new=mock
+        ):
+            async for v in account_proxy.mined_blocks_generator(
+                address="address",
+                blocktype='blocks',
+                page=1,
+                offset=10_000,
+            ):
+                pass
+            mock.assert_called_once_with(
+                client.account.mined_blocks,
+                address="address",
+                blocktype='blocks',
+                page=1,
+                offset=10_000,
+            )
+
+    @pytest.mark.asyncio
+    async def test_normal_txs_generator(self, client, account_proxy):
+        mock = MagicMock()
+        with asynctest.patch(
+            'aioetherscan.modules.utils.BaseProxy.parse_by_blocks',
+            new=mock
+        ):
+            async for v in account_proxy.normal_txs_generator(
+                address="address",
+                start_block=0,
+                end_block=None,
+                sort="asc",
+                page=1,
+                offset=10_000,
+            ):
+                pass
+            mock.assert_called_once_with(
+                client.account.normal_txs,
+                address="address",
+                start_block=0,
+                end_block=None,
+                sort="asc",
+                page=1,
+                offset=10_000,
+            )
+
+    @pytest.mark.asyncio
+    async def test_internal_txs_generator(self, client, account_proxy):
+        mock = MagicMock()
+        with asynctest.patch(
+            'aioetherscan.modules.utils.BaseProxy.parse_by_blocks',
+            new=mock
+        ):
+            async for v in account_proxy.internal_txs_generator(
+                address="address",
+                start_block=0,
+                end_block=None,
+                sort="asc",
+                page=1,
+                offset=10_000,
+                txhash="0x",
+            ):
+                pass
+            mock.assert_called_once_with(
+                client.account.internal_txs,
+                address="address",
+                start_block=0,
+                end_block=None,
+                sort="asc",
+                page=1,
+                offset=10_000,
+                txhash="0x",
+            )
+
+    @pytest.mark.asyncio
+    async def test_token_transfers_generator(self, client, account_proxy):
+        mock = MagicMock()
+        with asynctest.patch(
+            'aioetherscan.modules.utils.BaseProxy.parse_by_blocks',
+            new=mock
+        ):
+            async for v in account_proxy.token_transfers_generator(
+                address="address",
+                start_block=0,
+                end_block=None,
+                sort="asc",
+                page=1,
+                offset=10_000,
+            ):
+                pass
+            mock.assert_called_once_with(
+                client.account.token_transfers,
+                address="address",
+                start_block=0,
+                end_block=None,
+                sort="asc",
+                page=1,
+                offset=10_000,
+            )
+
+    @pytest.mark.asyncio
+    async def test_one_of_addresses_is_supplied(self, account_proxy):
+        EXC_MESSAGE_RE = r'At least one of address or contract_address must be specified.'
+        with pytest.raises(ValueError, match=EXC_MESSAGE_RE):
+            async for _ in account_proxy.token_transfers_generator(end_block=1):
+                break
