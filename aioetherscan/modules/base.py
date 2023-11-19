a6352fef4457024545e3ddcbfdf6a0cca4817569
ape364 <ape364@gmail.com>
2023-11-20 02:17:02 +0500

    remove unused method

--- aioetherscan/modules/base.py
+++ aioetherscan/modules/base.py
@@ -1,5 +1,4 @@
 from abc import ABC, abstractmethod
-from typing import Tuple
 
 
 class BaseModule(ABC):
@@ -16,9 +15,3 @@ class BaseModule(ABC):
 
     async def _post(self, **params):
         return await self._client._http.post(data={**dict(module=self._module), **params})
-
-    @staticmethod
-    def _check(value: str, values: Tuple[str, ...]):
-        if value and value.lower() not in values:
-            raise ValueError(f'Invalid value {value!r}, only {values} are supported.')
-        return value
