0d37af79692f109c8ceb1fb2154ada17fd570c66
ape364 <ape364@gmail.com>
2023-11-20 02:25:14 +0500

    refactor daily stats methods

--- aioetherscan/modules/block.py
+++ aioetherscan/modules/block.py
@@ -37,53 +37,32 @@ class Block(BaseModule):
             closest=check_closest_value(closest)
         )
 
-    async def daily_average_block_size(self, start_date: date, end_date: date, sort: Optional[str] = None) -> Dict:
-        """Get Daily Average Block Size"""
+    async def _get_daily_stats(self, action: str, start_date: date, end_date: date, sort: str) -> Dict:
         return await self._get(
             module='stats',
-            action='dailyavgblocksize',
+            action=action,
             startdate=start_date.isoformat(),
             enddate=end_date.isoformat(),
             sort=check_sort_direction(sort)
         )
+
+    async def daily_average_block_size(self, start_date: date, end_date: date, sort: Optional[str] = None) -> Dict:
+        """Get Daily Average Block Size"""
+        return await self._get_daily_stats('dailyavgblocksize', start_date, end_date, sort)
 
     async def daily_block_count(self, start_date: date, end_date: date, sort: Optional[str] = None) -> Dict:
         """Get Daily Block Count and Rewards"""
-        return await self._get(
-            module='stats',
-            action='dailyblkcount',
-            startdate=start_date.isoformat(),
-            enddate=end_date.isoformat(),
-            sort=check_sort_direction(sort)
-        )
+        return await self._get_daily_stats('dailyblkcount', start_date, end_date, sort)
 
     async def daily_block_rewards(self, start_date: date, end_date: date, sort: Optional[str] = None) -> Dict:
         """Get Daily Block Rewards"""
-        return await self._get(
-            module='stats',
-            action='dailyblockrewards',
-            startdate=start_date.isoformat(),
-            enddate=end_date.isoformat(),
-            sort=check_sort_direction(sort)
-        )
+        return await self._get_daily_stats('dailyblockrewards', start_date, end_date, sort)
 
     async def daily_average_time_for_a_block(self, start_date: date, end_date: date,
                                              sort: Optional[str] = None) -> Dict:
         """Get Daily Average Time for A Block to be Included in the Ethereum Blockchain"""
-        return await self._get(
-            module='stats',
-            action='dailyavgblocktime',
-            startdate=start_date.isoformat(),
-            enddate=end_date.isoformat(),
-            sort=check_sort_direction(sort)
-        )
+        return await self._get_daily_stats('dailyavgblocktime', start_date, end_date, sort)
 
     async def daily_uncle_block_count(self, start_date: date, end_date: date, sort: Optional[str] = None) -> Dict:
         """Get Daily Uncle Block Count and Rewards"""
-        return await self._get(
-            module='stats',
-            action='dailyuncleblkcount',
-            startdate=start_date.isoformat(),
-            enddate=end_date.isoformat(),
-            sort=check_sort_direction(sort)
-        )
+        return await self._get_daily_stats('dailyuncleblkcount', start_date, end_date, sort)
